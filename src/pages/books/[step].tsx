a9105b1d629fbfef74e3776f1d46c2d4a64e1e61
asst2506098 <asst2506098@tosspartners.com>
2025-08-30 02:22:36 +0900

    feat: 단계별 다음 버튼 유효성 검사 필드, 로직 추가

--- src/pages/books/[step].tsx
+++ src/pages/books/[step].tsx
@@ -3,40 +3,64 @@ import { useBookFormRouter } from "../../features/bookForm/hooks/useBookFormRout
 import { BOOK_FORM_STEPS } from "../../features/bookForm/constants/steps";
 import { getStepMetaByIndex } from "@/features/bookForm/utils/steps";
 import { FormProvider, useForm } from "react-hook-form";
+import { SubmitFormData } from "@/features/bookForm/types/basicStep";
+import { TOTAL_STEPS } from "../../features/bookForm/constants/steps";
 
 export default function NewBookStepPage() {
   const nav = useBookFormRouter();
-  const methods = useForm({ mode: "onChange" });
+  const methods = useForm<SubmitFormData>({
+    mode: "onChange",
+    defaultValues: {
+      bookTitle: "",
+      author: "",
+      publisher: "",
+      publishedDate: "",
+      totalPages: 0,
+      readingStatus: "want_to_read",
+      readingStartDate: "",
+      readingEndDate: "",
+      isRecommended: false,
+      rating: 0,
+      review: "",
+      quotes: [],
+      isPublic: false,
+    },
+  });
 
   const current = nav.stepIndex + 1;
-  const total = BOOK_FORM_STEPS.length;
   const CurrentStep = getStepMetaByIndex(nav.stepIndex)?.component ?? null;
+  const handleNext = async () => {
+    const isValid = await methods.trigger(
+      BOOK_FORM_STEPS[nav.stepIndex].fields
+    );
+    if (isValid) {
+      nav.goNext();
+    }
+  };
 
   return (
     <FormProvider {...methods}>
-      <main>
-        <BookFormLayout>
-          <BookFormLayout.Header
-            current={current}
-            total={total}
-            title={nav.meta.title}
-            description={nav.meta.description}
-          />
-          <BookFormLayout.Content>
-            {CurrentStep ? <CurrentStep /> : null}
-          </BookFormLayout.Content>
-          <BookFormLayout.Footer
-            isFirst={nav.isFirst}
-            isLast={nav.isLast}
-            onFirst={nav.goFirst}
-            onPrev={nav.goPrev}
-            onNext={nav.goNext}
-            onSubmit={() => {
-              alert("제출");
-            }}
-          />
-        </BookFormLayout>
-      </main>
+      <BookFormLayout>
+        <BookFormLayout.Header
+          current={current}
+          total={TOTAL_STEPS}
+          title={nav.meta.title}
+          description={nav.meta.description}
+        />
+        <BookFormLayout.Content>
+          {CurrentStep && <CurrentStep />}
+        </BookFormLayout.Content>
+        <BookFormLayout.Footer
+          isFirst={nav.isFirst}
+          isLast={nav.isLast}
+          onFirst={nav.goFirst}
+          onPrev={nav.goPrev}
+          onNext={handleNext}
+          onSubmit={() => {
+            alert("제출");
+          }}
+        />
+      </BookFormLayout>
     </FormProvider>
   );
 }
