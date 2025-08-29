9cac5fb0f661687d3660ea45f9f1e65e37aabe67
asst2506098 <asst2506098@tosspartners.com>
2025-08-30 01:20:30 +0900

    feat: FormProvider 설정

--- src/pages/books/[step].tsx
+++ src/pages/books/[step].tsx
@@ -2,37 +2,41 @@ import { BookFormLayout } from "../../features/bookForm/components/layout/BookFo
 import { useBookFormRouter } from "../../features/bookForm/hooks/useBookFormRouter";
 import { BOOK_FORM_STEPS } from "../../features/bookForm/constants/steps";
 import { getStepMetaByIndex } from "@/features/bookForm/utils/steps";
+import { FormProvider, useForm } from "react-hook-form";
 
 export default function NewBookStepPage() {
   const nav = useBookFormRouter();
+  const methods = useForm({ mode: "onChange" });
 
   const current = nav.stepIndex + 1;
   const total = BOOK_FORM_STEPS.length;
   const CurrentStep = getStepMetaByIndex(nav.stepIndex)?.component ?? null;
 
   return (
-    <main>
-      <BookFormLayout>
-        <BookFormLayout.Header
-          current={current}
-          total={total}
-          title={nav.meta.title}
-          description={nav.meta.description}
-        />
-        <BookFormLayout.Content>
-          {CurrentStep ? <CurrentStep /> : null}
-        </BookFormLayout.Content>
-        <BookFormLayout.Footer
-          isFirst={nav.isFirst}
-          isLast={nav.isLast}
-          onFirst={nav.goFirst}
-          onPrev={nav.goPrev}
-          onNext={nav.goNext}
-          onSubmit={() => {
-            alert("제출");
-          }}
-        />
-      </BookFormLayout>
-    </main>
+    <FormProvider {...methods}>
+      <main>
+        <BookFormLayout>
+          <BookFormLayout.Header
+            current={current}
+            total={total}
+            title={nav.meta.title}
+            description={nav.meta.description}
+          />
+          <BookFormLayout.Content>
+            {CurrentStep ? <CurrentStep /> : null}
+          </BookFormLayout.Content>
+          <BookFormLayout.Footer
+            isFirst={nav.isFirst}
+            isLast={nav.isLast}
+            onFirst={nav.goFirst}
+            onPrev={nav.goPrev}
+            onNext={nav.goNext}
+            onSubmit={() => {
+              alert("제출");
+            }}
+          />
+        </BookFormLayout>
+      </main>
+    </FormProvider>
   );
 }
